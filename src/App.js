915b37630150223dc07f5fe612abe33ef8f29eea
M2M <M2M@M2M-Mac.local>
2019-12-07 14:46:17 +0600

    Added ColorHelper to Generate Colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import Palette from './Palette';
+import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
         <Palette {...seedColors[2]}/>
