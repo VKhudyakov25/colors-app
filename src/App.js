87c7dd39d70477ddccb459e8e341910ed0741c91
M2M <M2M@M2M-Mac.local>
2019-12-09 17:12:31 +0600

    Fixed palette Route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors';
 
 class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find( (palette) => {
+      return palette.id === id}
+  )}
+
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>List of palettes</h1>}/>
-        <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>}/>
+        <Route exact path="/palette/:id" render={(routeProps) => (
+          <Palette 
+            palette = {
+              generatePalette(this.findPalette(routeProps.match.params.id)
+              )}
+          />
+        )}/>
       </Switch>
       
       // <div>
