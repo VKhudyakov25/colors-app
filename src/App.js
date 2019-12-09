b91cecb0e393b1f0f6a3f741eda71ec942d528e5
M2M <M2M@M2M-Mac.local>
2019-12-09 16:57:47 +0600

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import Palette from './Palette';
+import { Route, Switch } from 'react-router-dom';
 import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])}/>
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>List of palettes</h1>}/>
+        <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>}/>
+      </Switch>
+      
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])}/>
+      // </div>
     )
   }
 }
