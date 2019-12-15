b62e571e9ab8d95ddc1ceb62ce80a37be41cde40
M2M <M2M@M2M-Mac.local>
2019-12-15 14:13:40 +0600

    Added DraggableColorBox Component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,6 +11,7 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import DraggableColorBox from './DraggableColorBox';
 import { ChromePicker } from 'react-color';
 
 
@@ -56,6 +57,7 @@ const styles = theme => ({
   },
   content: {
     flexGrow: 1,
+    height: 'calc(100vh - 63px)',
     padding: theme.spacing(3),
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
@@ -174,11 +176,9 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          <ul>
             {this.state.colors.map(color => (
-              <li style={{backgroundColor: color}}> {color} </li>
+              <DraggableColorBox color={color}/>
             ))}
-          </ul>
         </main>
       </div>
     );
