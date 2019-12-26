892c99d0a52538a86239159fc43e7de31fd8f1ea
M2M <M2M@M2M-Mac.local>
2019-12-26 16:58:03 +0600

    Added local storage setting

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ import NewPaletteForm from './NewPaletteForm';
 class App extends Component {
   constructor(props){
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: seedColors
+      palettes: savedPalettes || seedColors
     }
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.syncLocalStorage = this.syncLocalStorage.bind(this);
   }
 
   savePalette(palette){
     this.setState({
       palettes: [...this.state.palettes, palette ]
-    })
+    }, this.syncLocalStorage)
+  }
+
+  syncLocalStorage(){
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
   }
 
   findPalette(id) {
